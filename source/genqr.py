fc9909f0b1d34b3b2be9241aba9dc694a067c66b
root <root@srv679218.hstgr.cloud>
2026-03-02 13:10:28 +0000

    Initial commit from Unix server

---
+++ source/genqr.py
@@ -0,0 +1,23 @@
+import qrcode
+
+# Text to encode
+data = "QROE4QMQXBJ2"
+
+# Generate QR code
+qr = qrcode.QRCode(
+    version=1,  # controls size of the QR code (1 = smallest)
+    error_correction=qrcode.constants.ERROR_CORRECT_L,  # error correction level
+    box_size=10,  # size of each box in pixels
+    border=4,  # thickness of the border
+)
+
+qr.add_data(data)
+qr.make(fit=True)
+
+# Create an image from the QR Code instance
+img = qr.make_image(fill_color="black", back_color="white")
+
+# Save the image
+img.save("Football Stage.png")
+
+print("QR code generated and saved as qrcode.png")
